8599f4a74f3d563778e583196dece16659f864fc
cryptobox <pray.happy76@gmail.com>
2025-10-27 21:01:14 +0900

    add

--- abc195/b/main.py
+++ abc195/b/main.py
@@ -1,14 +1,12 @@
 # !/usr/bin/env python3
-import math
 
 A, B, _W = map(int, input().split())
 W = _W * 1000
-ans = []
 
-for i in range(A, B + 1):
-	for x in range(0, 1001):
-		y = (W - A * x) / B
-		if y.is_integer():
-			ans.append([x, y])
+minN = (W + B - 1) // B
+maxN = W // A
 
-print(ans[0])
+if minN > maxN:
+    print("UNSATISFIABLE")
+else:
+    print(minN, maxN)
